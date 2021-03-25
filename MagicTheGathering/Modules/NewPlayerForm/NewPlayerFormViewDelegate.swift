a880d01100c3342aa3e9152b1b962d80458923ef
Luisma Tolosana <ltolosana@gmail.com>
2021-03-26 00:09:49 +0100

    Checking password requirements (min length, uppercase and number)

--- MagicTheGathering/Modules/NewPlayerForm/NewPlayerFormViewDelegate.swift
+++ MagicTheGathering/Modules/NewPlayerForm/NewPlayerFormViewDelegate.swift
@@ -39,8 +39,12 @@ class NewPlayerFormViewDelegate: NSObject, UITextFieldDelegate {
         case 5:
             return checkSameTextInput(textField, vs: .email)
         case 6:
-            return checkSameTextInput(textField, vs: .repeatPassword)
+            // Aqui hago una doble comprobacion, la de los requerimientos de la contraseña (cantidad de caracteres,
+            // mayuscula y numero) y la de que coincida la contraseña con la del campo de repetirla
+            return checkPasswordField(textField, vs: .repeatPassword)
         case 7:
+            // En el simulador no van muy bien las pruebas de esto y para poder probar
+            // hay que deshabilitar la opcion de "Secure Text Entry'en el textfield del .xib
             return checkSameTextInput(textField, vs: .password)
         default:
             return true
@@ -60,10 +64,15 @@ class NewPlayerFormViewDelegate: NSObject, UITextFieldDelegate {
 
 // MARK: - Extension NewPlayerFormViewDelegate
 extension NewPlayerFormViewDelegate {
+    
+    private func checkPasswordField(_ textField: UITextField, vs otherField: TextFieldType) -> Bool {
+        let contraints = checkPasswordConstraints(textField)
+        let sameText = checkSameTextInput(textField, vs: .repeatPassword)
         
+        return contraints && sameText
+    }
+    
     private func checkSameTextInput(_ textField: UITextField, vs otherField: TextFieldType) -> Bool {
-        let originalBackgroundColor = UIColor.white
-        let errorBackgroundColor = UIColor.systemRed.withAlphaComponent(0.2)
         
         guard let otherTextField = textField.superview?.superview?.viewWithTag(otherField.rawValue) as? UITextField else {
             return true
@@ -71,43 +80,26 @@ extension NewPlayerFormViewDelegate {
         
         // Si ambos campos son iguales limpamos el fondo y salimos
         if textField.text == otherTextField.text {
-            textField.leftViewMode = .never
-            textField.backgroundColor = originalBackgroundColor
-            otherTextField.leftViewMode = .never
-            otherTextField.backgroundColor = originalBackgroundColor
+            clearTextFieldError(textField)
+            clearTextFieldError(otherTextField)
         } else {
             // Si no son iguales ...
             switch otherField {
             case .email, .password:
                 // ... y estamos en uno de los campos de repeticion, nos ponemos en rojo y dejamos salir
                 // porque puede que el error este en el otro campo y asi el usuario podra arreglarlo
-                // Para que solo cree el overlayButton la primera vez
-                textField.leftView = textField.leftView ?? createOverlayButton()
-                textField.leftViewMode = .always
-                textField.backgroundColor = errorBackgroundColor
+                setTextFieldError(textField)
             case .repeatEmail, .repeatPassword:
                 // ... y estamos en uno de los campos principales, ponemos el de repeticion en rojo y salimos
                 // Salvo que el campo de repeticion este vacio, en cuyo caso le damos al usuario la oportunidad de rellenarlo
                 if otherTextField.text != "" {
-                    // Para que solo cree el overlayButton la primera vez
-                    otherTextField.leftView = otherTextField.leftView ?? createOverlayButton()
-                    otherTextField.leftViewMode = .always
-                    otherTextField.backgroundColor = errorBackgroundColor
+                    setTextFieldError(otherTextField)
                 }
             }
         }
         return true
     }
     
-    private func createOverlayButton() -> UIButton {
-        let overlayButton = UIButton(type: .custom)
-        overlayButton.setImage(UIImage(systemName: "exclamationmark.circle"), for: .normal)
-        overlayButton.tintColor = .systemRed
-        overlayButton.sizeToFit()
-        
-        return overlayButton
-    }
-
     private func checkPhoneNumber(_ textField: UITextField,
                                   shouldChangeCharactersIn range: NSRange,
                                   replacementString string: String) -> Bool {
@@ -155,5 +147,63 @@ extension NewPlayerFormViewDelegate {
                 }
             }
         }
+    }
+    
+    private func checkPasswordConstraints(_ textField: UITextField) -> Bool {
+        let minCharacters = 8 // Minimo numero de caracteres permitidos en la password
+        
+        guard let textToCheck = textField.text else {
+            return true
+        }
+        let range = NSRange(location: 0, length: textToCheck.utf16.count)
+        
+        // Nota: En estos dos casos concretos, me puedo permitir poner la "!" porque se seguro que
+        // la expresion regular que he puesto, funciona correctamente y ademas si por algun motivo
+        // un error al escribir codigo hace que se cambie la expresion,
+        // nos serviria para darnos cuenta del error porque "petaria" la simulacion
+        
+        // swiftlint:disable:next force_try
+        let regexUpper = try! NSRegularExpression(pattern: "[A-Z]")
+        // swiftlint:disable:next force_try
+        let regexNumbers = try! NSRegularExpression(pattern: "[0-9]")
+        
+        if range.length >= minCharacters &&
+            regexUpper.firstMatch(in: textToCheck, options: [], range: range) != nil &&
+            regexNumbers.firstMatch(in: textToCheck, options: [], range: range) != nil {
+            // Comprobamos que hay minimo 8 caracteres, ha encontrado al menos una mayuscula y al menos un numero,
+            // asi que limpiamos un posible error anterior y dejamos salir
+            clearTextFieldError(textField)
+            return true
+        } else {
+            // En este caso falta o mayuscula o numero asi que no dejamos salir
+            setTextFieldError(textField)
+            return false
+        }
+    }
+    
+    // MARK: - Auxiliary methods
+    private func setTextFieldError(_ textField: UITextField) {
+        let errorBackgroundColor = UIColor.systemRed.withAlphaComponent(0.2)
+        
+        // Para que solo cree el overlayButton la primera vez
+        textField.leftView = textField.leftView ?? createOverlayButton()
+        textField.leftViewMode = .always
+        textField.backgroundColor = errorBackgroundColor
+    }
+    
+    private func clearTextFieldError(_ textField: UITextField) {
+        let originalBackgroundColor = UIColor.white
+        
+        textField.leftViewMode = .never
+        textField.backgroundColor = originalBackgroundColor
+    }
+    
+    private func createOverlayButton() -> UIButton {
+        let overlayButton = UIButton(type: .custom)
+        overlayButton.setImage(UIImage(systemName: "exclamationmark.circle"), for: .normal)
+        overlayButton.tintColor = .systemRed
+        overlayButton.sizeToFit()
+        
+        return overlayButton
     }
 }
